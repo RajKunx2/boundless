19424960728fdc7d77e813b98ca6780540d89c98
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-06-09 16:27:53 +0530

    damsn

--- boundless/src/components/HomePage.jsx
+++ boundless/src/components/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
         <div className='flex flex-col lg:flex-row mt-[100px] justify-center items-center align-middle py-6'>
             <div className='flex flex-col md:mx-12 basis-1/3 justify-center items-center'>
                 <h1 className='font-bold text-orange-400 text-7xl my-2'>&#x23;freePads</h1>
-                <p><button className='shadow-2xl rounded-lg px-4 py-2 text-base my-2 text-orange-400 border-orange-200 hover:text-white hover:bg-orange-500 border-2'>ADVERTISE WITH US</button></p>
+                <p><button className='shadow-2xl rounded-lg px-4 py-2 text-base my-2 text-orange-400 border-orange-200 hover:text-white hover:bg-orange-500 border-2'><a href="https://hm-boundless.vercel.app">ADVERTISE WITH US</a></button></p>
             </div>
             <div className='flex flex-col md:mx-4 basis-1/3'>
                 <Image src={Main} alt='' height={500} width={500} />
